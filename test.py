41726644eeed48a84ecaf69b73799a8ad13569e8
宋明阳 <songmingyang@Kira.local>
2016-08-15 17:12:57 +0800

    Put

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
+import time
+
 li = [lambda:x for x in range(10)]
 print(li[0]())
 print(li[2]())
+
+
+def time_machine(func):
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        print(u'共耗时:%s秒' % (time.time()-start_time))
+    return wrapper()
+
+@time_machine
+def foo():
+    time.sleep(3)
+
+
+
+
 
